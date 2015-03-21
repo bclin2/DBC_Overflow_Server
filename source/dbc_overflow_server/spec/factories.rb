db106e848609e9f187dd3f7fed32228b8c2c0758
Brian Lin <brian.c.lin2@gmail.com>
2015-03-20 18:40:40 -0700

    finished most rspec testing for json returns

--- source/dbc_overflow_server/spec/factories.rb
+++ source/dbc_overflow_server/spec/factories.rb
@@ -2,9 +2,15 @@ require 'factory_girl'
 require 'faker'
 
 FactoryGirl.define do
-  factory :question do
-    title {Faker::Lorem.word}
-    content {Faker::Lorem.sentence}
+  factory :question do |f|
+    f.title {Faker::Lorem.word}
+    f.content {Faker::Lorem.sentence}
+
+    factory :question_with_answer do
+      after(:create) do |question|
+        create(:answer, question: question)
+      end
+    end
   end
 
   factory :answer do
