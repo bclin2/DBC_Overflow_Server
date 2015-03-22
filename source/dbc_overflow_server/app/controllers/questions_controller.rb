495fc5f5cf7022c257b010f32d5ea30391f5d733
Brian Lin <brian.c.lin2@gmail.com>
2015-03-21 21:30:22 -0700

    added new age; doesn't do anything

--- source/dbc_overflow_server/app/controllers/questions_controller.rb
+++ source/dbc_overflow_server/app/controllers/questions_controller.rb
@@ -78,10 +78,11 @@ private
   end
 
   def authorize_cors
-    headers['Access-Control-Allow-Origin'] = '*'
+    headers['Access-Control-Allow-Origin'] = 'http://localhost:8888'
     headers['Access-Control-Allow-Methods'] = 'POST, PUT, DELETE, GET, OPTIONS'
     headers['Access-Control-Request-Method'] = '*'
     headers['Access-Control-Allow-Headers'] = 'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+    headers['Access-Control-Max-Age'] = '1728000'
   end
 
 end
